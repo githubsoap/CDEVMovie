27580de424aa93ccdcf604c0d942f0a34e9ed34d
githubsoap <88619951+githubsoap@users.noreply.github.com>
2022-07-12 15:04:56 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
-    user: 'root',
-    password: 'cdev',
+    user: 'admin',
+    password: 'password',
     database: 'movie_info'
 });
 
